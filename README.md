8354061335747f546fbc5ce6a8ae60397a5d9da2
unknown <vandyliu1999@gmail.com>
2019-10-16 23:59:03 -0700

    second commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Following: https://realpython.com/flask-by-example-part-1-project-setup/
+
+Using env vars: https://stackoverflow.com/questions/57063609/exporting-environment-variables-for-flask-application-in-windows
--- app.py
+++ app.py
@@ -1,10 +1,21 @@
+import os
 from flask import Flask
-app = Flask(__name__)
+from dotenv import load_dotenv
 
+load_dotenv()
+
+app = Flask(__name__)
+app.config.from_object(os.getenv('APP_SETTINGS', None))
+print(os.getenv('APP_SETTINGS', None))
 
 @app.route('/')
 def hello():
     return "Hello World!"
+
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
 
 if __name__ == '__main__':
     app.run()
